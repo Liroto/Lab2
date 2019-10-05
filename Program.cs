88547a4458ec3b3d99888338c786ed46b679eb1c
Liroto <ololoshin228@gmail.com>
2019-10-05 16:45:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int a, b, c;
+            double discriminant, x1,x2;
+            Console.WriteLine("Введите индексы a,b,c для квадратного классического уравнения:");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            discriminant = b*b - 4*a*c;
+            if (discriminant < 0)
+            {
+            Console.WriteLine("Дискриминант меньше 0,у данного уравнения корней нет.");
+            }
+            else if (discriminant == 0)
+            {
+            x1 = -b / 2 * a;
+            Console.WriteLine("Дискриминант равен 0,у данного уравнения один корень.");
+            Console.WriteLine($"X = {x1}");
+            }
+            else {
+            x1 = (-b + Math.Sqrt(discriminant)) / 2 * a;
+            x2 = (-b - Math.Sqrt(discriminant)) / 2 * a;
+            Console.WriteLine("Дискриминант больше 0,у данного уравнения два корня.");
+            Console.WriteLine($"X1 = {x1};\tX2 = {x2}");
+            }*/ //1-я задача
+
+
+            /*//π/4 = 1 - 1/3 + 1/5 - 1/7 + 1/9...
+            double divided_pi = 0; int denominator = 1;
+            double counter;
+            counter = int.Parse(Console.ReadLine());
+            for (int i = 1; i < counter + 1; i++)
+            {
+                divided_pi += (double)1 / denominator;
+                Console.WriteLine($"{i}-я итерация,шаг 1: 1/{denominator} = {divided_pi}");
+                denominator += 2;
+                Console.WriteLine($"{i}-я итерация,шаг 2: 1/{denominator}");
+                divided_pi -= (double)1 / denominator;
+                Console.WriteLine($"{i}-я итерация,шаг 3: 1 - 1/{denominator} = {divided_pi}");
+                denominator += 2;
+            }
+            double pi = (double)divided_pi * 4;
+            Console.WriteLine(pi);*/ //2 - я задача
+
+            //1,1,2,3,5,8,13,21
+
+            /*int a = 0;
+            int b = 1;
+            for (; a < 10000; )
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+                if (a > 999 && a < 10000) {
+                    Console.Write($"{a} ");
+                }
+            }*/ //3-я задача
+
+            /*int N;
+            Console.Write("Введите число для разложения ");
+            N = int.Parse(Console.ReadLine());
+
+            if (N <= 0)
+            {
+                Console.Write("N должно быть натуральным числом ");
+            }
+            else
+            {
+                for (int x = 0; x < N; x++)
+                {
+                    for (int y = 0; y < N; y++)
+                    {
+                        for (int z = 0; z < N; z++)
+                        {
+                            if (N == Math.Pow(x, 3) + Math.Pow(y, 3) + Math.Pow(z, 3))
+                            {
+                                Console.Write($"{x},{y},{z} или ");
+                            }
+                        }
+                    }
+                }
+            }5-я задача*/
+
+            /*Console.Write("Введите возраст от 1 до 100 ");
+            int age = int.Parse(Console.ReadLine());
+            if (age < 0 || age > 100)
+            {
+                Console.WriteLine("Введено неправильное значение");
+            }
+            else {
+                if (age % 10 == 1)
+                {
+                    Console.Write($"{age} год ");
+                }
+                else if (age % 10 > 1 && age % 10 < 5)
+                {
+                    Console.Write($"{age} года ");
+                }
+                else {
+                    Console.Write($"{age} лет ");
+                }
+            } 6-я задача*/
+
+            /*double a, b, c;
+            Console.Write("введите стороны треугольника: ");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            if (a < 0 || b < 0 || c < 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else if (a == b || b == c || a == c)
+            {
+                Console.WriteLine("True");
+            }
+            else {
+                Console.WriteLine("False");
+            } 1-я индивидуальная задача*/
+
+
+            /*for (int i = 100; i < 1000; i++) {
+                if (i / 100 == i%10) {
+                    Console.WriteLine(i);
+                }
+            } 2-я индивидальная задача*/
+        }
+    }
+}
